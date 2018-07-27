c285bbec99c814df39fed7f7ad05432a5cd61949
AhmadAzadzadeh <a.azadzadeh.aa14@gmail.com>
2018-07-27 13:58:46 +0430

    Finish Up new Method and returning the form and start to create method

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,6 +1,13 @@
 defmodule Discuss.TopicController do
     use Discuss.Web, :controller
-    def new(conn, params) do
-        
+    def new(conn, _params) do
+        struct = %Discuss.Topic{}
+        params = %{}
+        changeset = Discuss.Topic.changeset(struct, params)
+        render conn, "new.html", changeset: changeset
+    end
+
+    def create(conn, params) do
+        %{"topic" => topic} = params
     end
 end
